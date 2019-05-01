82cfc86c4f5541adc7ba3b6294e620f157bf6eec
piotrkolanus <piotr.kolanus@gmail.com>
2019-05-01 18:19:41 +0200

    before extracting form to functional

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 
 class App extends Component {
   state = {
-    value: ''
+    showForm: false,
+    editWithWorkNotes: false, //do conditionally renerowania textarea work notes w functional compo
+    problemsArr: [],
+    form_data: {
+      // assignment_group: {
+      //   value: ''
+      // },
+      // short_description: {
+      //   value: 'null'
+      // }
+    }
   };
 
-  handleChange = e => {
-    this.setState({ value: e.target.value });
+  handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(name);
+    this.setState({
+      form_data: {
+        ...this.state.form_data,
+        [name]: {
+          ...this.state.form_data[name],
+          value
+        }
+      }
+    });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    alert('Description value: ' + this.state.value);
+    const form_dataCpy = { ...this.state.form_data };
+
+    console.log(Object.values(this.state.form_data).map(x => x.value));
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Description
+      <div className="wrapper">
+        {/* <h2>Enter problem data</h2> */}
+        <form className="form" name="form" onSubmit={this.handleSubmit}>
+          <label htmlFor="assignment_group">Assignment Group</label>
           <input
             type="text"
+            name="assignment_group"
             value={this.state.value}
             onChange={this.handleChange}
+            placeholder="Assignment Group"
           />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+          <label htmlFor="short_description">Short description</label>
+          <textarea
+            type="text"
+            name="short_description"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Short description"
+          />
+          <input className="form__submit" type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
